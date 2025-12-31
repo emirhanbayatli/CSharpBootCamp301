0138352bc5c808d15cd86a4a6bf60f35d10fe8be
Emirhan Bayatlı <emirhan.bayatli99@icloud.com>
2025-12-31 16:46:15 +0300

    Lesson 13 completed

--- CSharpBootCamp301.EntityLayer/Concrete/Customer.cs
+++ CSharpBootCamp301.EntityLayer/Concrete/Customer.cs
@@ -14,6 +14,6 @@ namespace CSharpBootCamp301.EntityLayer.Concrete
         public string CustomerDistrict{ get; set; }
         public string CustomerCity { get; set; }
         public List<Order> Orders { get; set; }
-
+        public bool CustomerStatus { get; set; }
     }
 }
