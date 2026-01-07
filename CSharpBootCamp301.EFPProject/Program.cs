a23c1ed853a7aef9a652b8b6787ebaa66e164c1f
Emirhan Bayatlı <emirhan.bayatli99@icloud.com>
2026-01-07 15:48:22 +0300

    Lesson 17 completed

--- CSharpBootCamp301.EFPProject/Program.cs
+++ CSharpBootCamp301.EFPProject/Program.cs
@@ -16,7 +16,7 @@ namespace CSharpBootCamp301.EFPProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmLocation());
+            Application.Run(new FrmStatistics());
         }
     }
 }
