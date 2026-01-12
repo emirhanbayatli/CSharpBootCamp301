9e138a14e4318c0da585b0e3b8540d66f39323cd
Emirhan Bayatlı <emirhan.bayatli99@icloud.com>
2026-01-12 14:35:00 +0300

    Lesson 18 completed

---
+++ CSharpBootCamp301.DataAccessLayer/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,15 @@
+﻿using CSharpBootCamp301.DataAccessLayer.Abstract;
+using CSharpBootCamp301.DataAccessLayer.Repositories;
+using CSharpBootCamp301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBootCamp301.DataAccessLayer.EntityFramework
+{
+    public class EfCategoryDal :GenericRepository<Category>,ICategoryDal
+    {
+    }
+}
