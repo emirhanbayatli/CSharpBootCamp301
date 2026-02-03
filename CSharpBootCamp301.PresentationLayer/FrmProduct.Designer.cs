ad2813bdaec303fbe39331edbad9953ecde33473
Emirhan Bayatlı <emirhan.bayatli99@icloud.com>
2026-02-03 16:25:08 +0300

    lesson 22 completed

--- CSharpBootCamp301.PresentationLayer/FrmProduct.Designer.cs
+++ CSharpBootCamp301.PresentationLayer/FrmProduct.Designer.cs
@@ -58,6 +58,7 @@
             this.btnGetById.TabIndex = 26;
             this.btnGetById.Text = "Get By ID";
             this.btnGetById.UseVisualStyleBackColor = true;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
             // 
             // btnUpdate
             // 
@@ -67,6 +68,7 @@
             this.btnUpdate.TabIndex = 25;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDelete
             // 
@@ -86,6 +88,7 @@
             this.btnAdd.TabIndex = 23;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // txtProductName
             // 
@@ -242,6 +245,7 @@
             this.Controls.Add(this.label1);
             this.Name = "FrmProduct";
             this.Text = "FrmProduct";
+            this.Load += new System.EventHandler(this.FrmProduct_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
